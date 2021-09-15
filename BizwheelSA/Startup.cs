847f92401fe4d5375ac615830968df5718f402f7
Isabelle Boshoff <isabelleboshoff@gmail.com>
2021-09-15 17:54:52 +0200

    first commit

---
+++ BizwheelSA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BizwheelSA.Startup))]
+namespace BizwheelSA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
